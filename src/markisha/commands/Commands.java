c6158b9833bab7c8cd1c4188f0d6a07c570d95d0
marko <marko.tanasic01@gmail.com>
2024-12-20 11:46:12 +0100

    updated deprecated methods

--- src/markisha/commands/Commands.java
+++ src/markisha/commands/Commands.java
@@ -3,32 +3,35 @@ package markisha.commands;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+
 public class Commands implements CommandExecutor {
 
 	public static Map<Player, Boolean> playerEnabled = new HashMap<>();
-	
-	private void setEnabledDisabled(Player player) {	
+
+	private void setEnabledDisabled(Player player) {
 		if (playerEnabled.containsKey(player)) {
 			playerEnabled.put(player, !playerEnabled.get(player).booleanValue());
 		} else {
 			playerEnabled.put(player, true);
 		}
 	}
-	
+
 	private void setEnabled(Player player) {
 		playerEnabled.put(player, true);
 	}
-	
+
 	private void setDisabled(Player player) {
 		playerEnabled.put(player, false);
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
@@ -36,7 +39,7 @@ public class Commands implements CommandExecutor {
 			Player player = (Player) sender;
 
 			if (cmd.getName().equalsIgnoreCase("tc")) {
-				
+
 				if (args.length > 0) {
 					if (args[0].equalsIgnoreCase("e") || args[0].equalsIgnoreCase("enable")) {
 						setEnabled(player);
@@ -46,11 +49,17 @@ public class Commands implements CommandExecutor {
 				} else {
 					setEnabledDisabled(player);
 				}
-				
+
 				if (playerEnabled.get(player).booleanValue())
-					player.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "(!)" + ChatColor.RESET + "" + ChatColor.YELLOW + " TreeCutter enabled.");
+					player.sendMessage(Component.text().content("(!)").color(NamedTextColor.YELLOW)
+							.decoration(TextDecoration.BOLD, true)
+							.append(Component.text().content(" TreeCutter enabled.").color(NamedTextColor.WHITE)
+							.decoration(TextDecoration.BOLD, false)));
 				else
-					player.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "(!)" + ChatColor.RESET + "" + ChatColor.YELLOW + " TreeCutter disabled.");
+					player.sendMessage(Component.text().content("(!)").color(NamedTextColor.YELLOW)
+							.decoration(TextDecoration.BOLD, true)
+							.append(Component.text().content(" TreeCutter disabled.").color(NamedTextColor.WHITE)
+							.decoration(TextDecoration.BOLD, false)));
 			}
 		}
 
--- src/markisha/treecutter/TreeCutter.java
+++ src/markisha/treecutter/TreeCutter.java
@@ -1,24 +1,24 @@
 package markisha.treecutter;
 
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import markisha.commands.Commands;
 import markisha.events.CutTree;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 public class TreeCutter extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		Commands commands = new Commands();
 		getServer().getPluginManager().registerEvents(new CutTree(), this);
-		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[TreeCutter]: Plugin enabled!");
-		getCommand("tc").setExecutor(commands);
+		getCommand("tc").setExecutor(new Commands());
+		getServer().getConsoleSender().sendMessage(Component.text().content("[TreeCutter]: Plugin enabled!").color(NamedTextColor.GREEN));
 	}
 	
 	@Override
 	public void onDisable() {
-		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[TreeCutter]: Plugin disabled!");
+		getServer().getConsoleSender().sendMessage(Component.text().content("[TreeCutter]: Plugin disabled!").color(NamedTextColor.GREEN));
 	}
 	
 }
