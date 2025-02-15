0be86b2b54240336781bbf17f3f2811e61798ce1
marko <marko.tanasic01@gmail.com>
2025-02-15 10:57:06 +0100

    util class init on startup

--- src/markisha/treecutter/TreeCutter.java
+++ src/markisha/treecutter/TreeCutter.java
@@ -1,9 +1,11 @@
 package markisha.treecutter;
 
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import markisha.commands.Commands;
 import markisha.events.CutTree;
+import markisha.util.WoodMaterialUtil;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 
@@ -12,7 +14,11 @@ public class TreeCutter extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(new CutTree(), this);
+		
 		getCommand("tc").setExecutor(new Commands());
+		if (WoodMaterialUtil.isWood(Material.OAK_LOG))
+			getServer().getConsoleSender().sendMessage(Component.text().content("[TreeCutter]: Initialized!").color(NamedTextColor.GREEN));
+		
 		getServer().getConsoleSender().sendMessage(Component.text().content("[TreeCutter]: Plugin enabled!").color(NamedTextColor.GREEN));
 	}
 	
